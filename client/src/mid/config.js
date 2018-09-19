467d79e5b71b96a98466fad0d1b33251c2d3fd17
jonatay <jono.stretch@gmail.com>
2018-09-19 15:00:27 +0200

    fixing config for client

--- client/src/mid/config.js
+++ client/src/mid/config.js
@@ -1,1 +1,9 @@
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCTqJiaMY5u-fj3B7p7_Ib1qqon255DKOU',
+  authDomain: 'react-linkup.firebaseapp.com',
+  databaseURL: 'https://react-linkup.firebaseio.com',
+  storageBucket: 'react-linkup.appspot.com'
+};
+
 export const analyticsId = 'UA-126041937-1';
--- client/src/mid/firebase/config.sample.js
+++
@@ -1,8 +0,0 @@
-// todo: set your listParams and rename config.js
-
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCUll5AyYba1XL8NDYKZ51RGt90KofQo6c',
-  authDomain: 'todo-redux-saga.firebaseapp.com',
-  databaseURL: 'https://todo-redux-saga.firebaseio.com',
-  storageBucket: 'todo-redux-saga.appspot.com'
-};
